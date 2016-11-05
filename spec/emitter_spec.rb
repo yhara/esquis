23211c40aebe2e05eb99179f2afbea38b7f2cca2
Yutaka HARA <yutaka.hara+github@gmail.com>
2016-11-05 16:47:10 +0900

    Copied tests from yhara/kareido

---
+++ spec/emitter_spec.rb
@@ -0,0 +1,160 @@
+require 'spec_helper'
+
+describe "ll emitter:" do
+  def to_ll(src)
+    ast = Esquis::Parser.new.parse(src)
+    ast.to_ll
+  end
+
+  describe "extern" do
+    it "should add the body to ll" do
+      ll = to_ll(<<~EOD)
+        extern i32 putchar(i32);
+      EOD
+      expect(ll).to eq(<<~EOD)
+        declare i32 @putchar(i32)
+        define i32 @main() {
+          ret i32 0
+        }
+      EOD
+    end
+  end
+
+  describe "main stmts" do
+    it "should be the body of @main" do
+      ll = to_ll(<<~EOD)
+        extern i32 putchar(i32);
+        putchar(65);
+      EOD
+      expect(ll).to eq(<<~EOD)
+        declare i32 @putchar(i32)
+        define i32 @main() {
+          %reg1 = fptosi double 65.0 to i32
+          %reg2 = call i32 @putchar(i32 %reg1)
+          %reg3 = sitofp i32 %reg2 to double
+          ret i32 0
+        }
+      EOD
+    end
+  end
+
+  describe "func definition" do
+    it "should define a function" do
+      ll = to_ll(<<~EOD)
+        func foo(x, y) { return x; }
+        foo(123, 456);
+      EOD
+      expect(ll).to eq(<<~EOD)
+        define double @foo(double %x, double %y) {
+          ret double %x
+          ret double 0.0
+        }
+        define i32 @main() {
+          %reg1 = call double @foo(double 123.0, double 456.0)
+          ret i32 0
+        }
+      EOD
+    end
+  end
+
+  describe "if stmt" do
+    it "true case" do
+      ll = to_ll(<<~EOD)
+        extern i32 putchar(i32);
+        if (1 < 2) {
+          putchar(65);
+        }
+      EOD
+      expect(ll).to eq(<<~EOD)
+        declare i32 @putchar(i32)
+        define i32 @main() {
+          %reg1 = fcmp olt double 1.0, 2.0
+          br i1 %reg1, label %Then1, label %EndIf1
+        Then1:
+          %reg2 = fptosi double 65.0 to i32
+          %reg3 = call i32 @putchar(i32 %reg2)
+          %reg4 = sitofp i32 %reg3 to double
+          br label %EndIf1
+        EndIf1:
+          ret i32 0
+        }
+      EOD
+    end
+  end
+
+  describe "for stmt" do
+    it "should expand into loop" do
+      ll = to_ll(<<~EOD)
+        extern i32 putchar(i32);
+        for (x; 65 ... 70 ; 2) {
+          putchar(x);
+        }
+      EOD
+      expect(ll).to eq(<<~EOD)
+        declare i32 @putchar(i32)
+        define i32 @main() {
+          br label %For1
+        For1:
+          br label %Loop1
+        Loop1:
+          %x = phi double [65.0, %For1], [%fori1, %ForInc1]
+          %forc1 = fcmp oge double %x, 70.0
+          br i1 %forc1, label %EndFor1, label %ForBody1
+        ForBody1:
+          %reg1 = fptosi double %x to i32
+          %reg2 = call i32 @putchar(i32 %reg1)
+          %reg3 = sitofp i32 %reg2 to double
+          br label %ForInc1
+        ForInc1:
+          %fori1 = fadd double %x, 2.0
+          br label %Loop1
+        EndFor1:
+          ret i32 0
+        }
+      EOD
+    end
+  end
+
+  describe "binary expr" do
+    describe "`+`" do
+      it "should conveted to add" do
+        ll = to_ll(<<~EOD)
+          extern i32 putchar(i32);
+          putchar(60 + 5);
+        EOD
+        expect(ll).to eq(<<~EOD)
+          declare i32 @putchar(i32)
+          define i32 @main() {
+            %reg1 = fadd double 60.0, 5.0
+            %reg2 = fptosi double %reg1 to i32
+            %reg3 = call i32 @putchar(i32 %reg2)
+            %reg4 = sitofp i32 %reg3 to double
+            ret i32 0
+          }
+        EOD
+      end
+    end
+  end
+
+  describe "unary expr" do
+    describe "-" do
+      it "should flip the sign" do
+        ll = to_ll(<<~EOD)
+          func foo(x) { return -x; }
+          foo(3);
+        EOD
+        expect(ll).to eq(<<~EOD)
+          define double @foo(double %x) {
+            %reg1 = fsub double 0.0, %x
+            ret double %reg1
+            ret double 0.0
+          }
+          define i32 @main() {
+            %reg2 = call double @foo(double 3.0)
+            ret i32 0
+          }
+        EOD
+      end
+    end
+  end
+end
---
+++ spec/esquis_spec.rb
@@ -0,0 +1,170 @@
+require 'spec_helper'
+
+MANDEL = <<EOD
+extern i32 putchar(i32);
+
+func printdensity(d) {
+  if (d > 8) {
+    putchar(32);  // ' '
+  }
+  else if (d > 4) {
+    putchar(46);  // '.'
+  }
+  else if (d > 2) {
+    putchar(43);  // '+'
+  }
+  else {
+    putchar(42); // '*'
+  }
+}
+
+func mandleconverger(real, imag, iters, creal, cimag) {
+  if (iters > 255 || (real*real + imag*imag > 4)) {
+    iters;
+  }
+  else {
+    mandleconverger(real*real - imag*imag + creal,
+                    2*real*imag + cimag,
+                    iters+1, creal, cimag);
+  }
+}
+
+func mandleconverge(real, imag) {
+  mandleconverger(real, imag, 0, real, imag);
+}
+
+func mandelhelp(xmin, xmax, xstep, ymin, ymax, ystep) {
+  for (y ; ymin ... ymax ; ystep) {
+    for (x ; xmin ... xmax ; xstep) {
+       printdensity(mandleconverge(x,y));
+    }
+    putchar(10);
+  }
+}
+
+func mandel(realstart, imagstart, realmag, imagmag) {
+  mandelhelp(realstart, realstart+realmag*78, realmag,
+             imagstart, imagstart+imagmag*40, imagmag);
+}
+
+mandel(-2.3, -1.3, 0.05, 0.07);
+EOD
+
+describe Esquis do
+  it 'should parse example program' do
+    ast = Esquis::Parser.new.parse(MANDEL)
+    expect(ast).to be_kind_of(Esquis::Ast::Node)
+  end
+
+  describe '.run' do
+    it 'should run esquis program (with llc)' do
+      out = Esquis.run(<<~EOD)
+        extern i32 putchar(i32);
+        putchar(65);
+      EOD
+      expect(out).to eq("A")
+    end
+  end
+
+  describe 'programs' do
+    it 'defun' do
+      src = <<-EOD
+        extern i32 putchar(i32);
+        func add(x, y){ return x + y; }
+        putchar(add(60, 5));
+      EOD
+      expect(Esquis.run(src)).to eq("A")
+    end
+
+    it '+' do
+      src = "extern i32 putchar(i32); putchar(60 + 5);"
+      expect(Esquis.run(src)).to eq("A")
+    end
+
+    it '-' do
+      src = "extern i32 putchar(i32); putchar(70 - 5);"
+      expect(Esquis.run(src)).to eq("A")
+    end
+
+    it '*' do
+      src = "extern i32 putchar(i32); putchar(13 * 5);"
+      expect(Esquis.run(src)).to eq("A")
+    end
+
+    it '/' do
+      src = "extern i32 putchar(i32); putchar(157 / 2.41);"
+      expect(Esquis.run(src)).to eq("A")
+    end
+
+    it '==' do
+      src = "extern i32 putchar(i32); if (1 == 1) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("A")
+      src = "extern i32 putchar(i32); if (1 == 2) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("")
+    end
+
+    it '>' do
+      src = "extern i32 putchar(i32); if (2 > 1) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("A")
+      src = "extern i32 putchar(i32); if (1 > 2) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("")
+    end
+
+    it '>=' do
+      src = "extern i32 putchar(i32); if (1 >= 1) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("A")
+      src = "extern i32 putchar(i32); if (1 >= 2) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("")
+    end
+
+    it '<' do
+      src = "extern i32 putchar(i32); if (1 < 2) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("A")
+      src = "extern i32 putchar(i32); if (2 < 1) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("")
+    end
+
+    it '<=' do
+      src = "extern i32 putchar(i32); if (1 <= 1) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("A")
+      src = "extern i32 putchar(i32); if (2 <= 1) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("")
+    end
+
+    it '!=' do
+      src = "extern i32 putchar(i32); if (1 != 2) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("A")
+      src = "extern i32 putchar(i32); if (1 != 1) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("")
+    end
+
+    it '&&' do
+      src = "extern i32 putchar(i32); if (1 == 1 && 2 == 2) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("A")
+      src = "extern i32 putchar(i32); if (1 == 1 && 2 == 0) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("")
+    end
+
+    it '||' do
+      src = "extern i32 putchar(i32); if (1 == 0 || 2 == 2) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("A")
+      src = "extern i32 putchar(i32); if (1 == 0 || 2 == 0) { putchar(65); }"
+      expect(Esquis.run(src)).to eq("")
+    end
+
+    it 'unary -' do
+      src = "extern i32 putchar(i32); putchar(-(-65));"
+      expect(Esquis.run(src)).to eq("A")
+    end
+
+    it 'for' do
+      src = <<-EOD
+        extern i32 putchar(i32);
+        for (x; 65 ... 70 ; 2) {
+          putchar(x);
+        }
+      EOD
+      expect(Esquis.run(src)).to eq("ACE")
+    end
+  end
+end
