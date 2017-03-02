6c97ed922a8eea0c7b60543bd35785fb24365ccd
Yutaka HARA <yutaka.hara+github@gmail.com>
2017-03-02 16:23:52 +0900

    refactor test (add context)

--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -59,74 +59,76 @@ describe "Esquis" do
       end
     end
 
-    it "should check arity when calling toplevel func" do
-      expect {
-        to_ll(<<~EOD)
-          def foo(x: Float) -> Float { 1; }
-          foo(1, 2);
-        EOD
-      }.to raise_error(Esquis::Ast::ArityMismatch)
-    end
-
-    it "should check arg types when calling toplevel func" do
-      expect {
-        to_ll(<<~EOD)
-          def foo(x: Float) -> Float { 1; }
-          foo(1 == 2);
-        EOD
-      }.to raise_error(Esquis::Ast::TypeMismatch)
-    end
-
-    it "should pass recursive toplevel func" do
-      expect {
-        to_ll(<<~EOD)
-          def foo(x: Float) -> Float { foo(x); }
-        EOD
-      }.not_to raise_error
-    end
-
-    it "should check arity of new" do
-      expect {
-        to_ll(<<~EOD)
-          class A
-            def initialize() { 1; }
-          end
-          A.new(1);
-        EOD
-      }.to raise_error(Esquis::Ast::ArityMismatch)
-    end
-
-    it "should check arg types of new" do
-      expect {
-        to_ll(<<~EOD)
-          class A
-            def initialize(x: Float) { 1; }
-          end
-          A.new(1 == 1);
-        EOD
-      }.to raise_error(Esquis::Ast::TypeMismatch)
-    end
-
-    it "should check arity of method call" do
-      expect {
-        to_ll(<<~EOD)
-          class A
-            def foo() -> Float { 1; }
-          end
-          A.new().foo(1);
-        EOD
-      }.to raise_error(Esquis::Ast::ArityMismatch)
-    end
-
-    it "should check arg types of method call" do
-      expect {
-        to_ll(<<~EOD)
-          class A
+    context "method/function call" do
+      it "should check arity when calling toplevel func" do
+        expect {
+          to_ll(<<~EOD)
             def foo(x: Float) -> Float { 1; }
-          end
-          A.new().foo(1 == 1);
-        EOD
-      }.to raise_error(Esquis::Ast::TypeMismatch)
+            foo(1, 2);
+          EOD
+        }.to raise_error(Esquis::Ast::ArityMismatch)
+      end
+
+      it "should check arg types when calling toplevel func" do
+        expect {
+          to_ll(<<~EOD)
+            def foo(x: Float) -> Float { 1; }
+            foo(1 == 2);
+          EOD
+        }.to raise_error(Esquis::Ast::TypeMismatch)
+      end
+
+      it "should pass recursive toplevel func" do
+        expect {
+          to_ll(<<~EOD)
+            def foo(x: Float) -> Float { foo(x); }
+          EOD
+        }.not_to raise_error
+      end
+
+      it "should check arity of new" do
+        expect {
+          to_ll(<<~EOD)
+            class A
+              def initialize() { 1; }
+            end
+            A.new(1);
+          EOD
+        }.to raise_error(Esquis::Ast::ArityMismatch)
+      end
+
+      it "should check arg types of new" do
+        expect {
+          to_ll(<<~EOD)
+            class A
+              def initialize(x: Float) { 1; }
+            end
+            A.new(1 == 1);
+          EOD
+        }.to raise_error(Esquis::Ast::TypeMismatch)
+      end
+
+      it "should check arity of method call" do
+        expect {
+          to_ll(<<~EOD)
+            class A
+              def foo() -> Float { 1; }
+            end
+            A.new().foo(1);
+          EOD
+        }.to raise_error(Esquis::Ast::ArityMismatch)
+      end
+
+      it "should check arg types of method call" do
+        expect {
+          to_ll(<<~EOD)
+            class A
+              def foo(x: Float) -> Float { 1; }
+            end
+            A.new().foo(1 == 1);
+          EOD
+        }.to raise_error(Esquis::Ast::TypeMismatch)
+      end
     end
 
     it "should check condition of if-stmt is Bool" do
